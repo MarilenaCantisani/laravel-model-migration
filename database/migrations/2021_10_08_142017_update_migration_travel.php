631f670f33e1d8a279c4ee90d8e1b38a2e8f54ca
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-10-08 16:37:49 +0200

    Added new migration

---
+++ database/migrations/2021_10_08_142017_update_migration_travel.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateMigrationTravel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('travels', function (Blueprint $table) {
+            // Create new data for the travel table 
+            $table->text('description');
+            $table->tinyInteger('duration');
+            $table->foreignId('id_transport');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('travels', function (Blueprint $table) {
+            // Remove the new fields created
+            $table->dropColumn('description');
+            $table->dropColumn('duration');
+            $table->dropColumn('id_transport');
+        });
+    }
+}
